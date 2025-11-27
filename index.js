3d20109892cc3d6b70b2ae0f30ae7965294b2d81
CziczasM <cziczas.martin@kreativszolnok.hu>
2025-11-27 11:22:38 +0100

    js

---
+++ index.js
@@ -0,0 +1,22 @@
+const quizQuestions = [
+    {
+        subject: "történelem",
+        //1
+        question: "Mikor tört ki Pesten a forradalom?",
+        answers: ["1848. március 12.", "1848. március 15.", "1848. április 14", "1848. június 1."],
+        corrects: 1,
+        //2
+        question: "Ki vezette a magyar honvédsereg főparancsnokságát a szabadságharc nagy részében?",
+        answers: ["Kossuth Lajos", "Görgei Artúr", "Bem József", "Batthyány Lajos"],
+        corrects: 1,
+        //3
+        question: "Hol fogalmazták meg a 12 pontot?",
+        answers: ["Pozsony", "Debrecen", "Buda", "Pest"],
+        corrects: 3,
+        //3+1
+        question: "Mi volt a szabadságharc végének szimbolikus dátuma (a világosi fegyverletétel)?",
+        answers: ["1849. május 1.", "1849. június 13.", "1849. augusztus 13.", "1849. október 6."],
+        corrects: 2
+    },
+    
+]
